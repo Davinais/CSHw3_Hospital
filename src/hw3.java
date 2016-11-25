7dec3b15c8a7e15b70505f7812535afa188496e6
Davinais <david870620@gmail.com>
2016-11-25 15:06:24 +0800

    Add some message

--- src/hw3.java
+++ src/hw3.java
@@ -10,15 +10,18 @@ public class hw3
         while(true)
         {
             nckuHospital.printStatus();
-            System.out.print(">>> ");
+            System.out.print("請輸入新病患需要的治療方法\n>>> ");
             String therapy = input.nextLine();
             if(therapy.equals("save"))
                 nckuHospital.saveHospital();
             else if(therapy.equals("load"))
                 nckuHospital = Hospital.loadHospital();
-            else if(!nckuHospital.dealWithIllness(therapy))
-                System.out.println("他轉學了");
-            nckuHospital.turnOver();
+            else 
+            {
+                if(!nckuHospital.dealWithIllness(therapy))
+                    System.out.println("不好意思，本醫院目前人手不足，可能要麻煩您轉院囉！");
+                nckuHospital.turnOver();
+            }
         }
     }
 }
