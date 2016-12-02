5bd7181ac484be15e80a8466d9031bd635a57299
Davinais <david870620@gmail.com>
2016-12-02 19:03:31 +0800

    Remove unnecessary parts

--- src/HospitalGUI.java
+++ src/HospitalGUI.java
@@ -128,13 +128,11 @@ class MedicStatusBox extends HBox
 }
 class MedicPane extends TilePane
 {
-    private MedicalPersonnel[] medics;
     private MedicStatusBox[] medicStats;
     private double prefStatusWidth = 140.0;
     public MedicPane(MedicalPersonnel[] medics)
     {
         super();
-        this.medics = medics;
         medicStats = new MedicStatusBox[medics.length];
         for(int i=0; i < medicStats.length; i++)
         {
@@ -151,34 +149,27 @@ class MedicPane extends TilePane
 }
 class MedicTab extends Tab
 {
-    private Hospital nckuHospital;
     private MedicPane medicPane;
-    private int job;
     public MedicTab(Hospital hospital, String jobName)
     {
         super();
-        nckuHospital = hospital;
         switch(jobName)
         {
             case "Surgeon":
                 setText("外科醫生");
-                job = 0;
                 break;
             case "Physician":
                 setText("內科醫生");
-                job = 1;
                 break;
             case "Nurse":
                 setText("護理師");
-                job = 2;
                 break;
             case "Anesthetist":
                 setText("麻醉師");
-                job = 3;
                 break;
             default:
         }
-        medicPane = new MedicPane(nckuHospital.getMedicList(jobName));
+        medicPane = new MedicPane(hospital.getMedicList(jobName));
         //建立捲動面板，會在視窗大小小於TilePane時出現捲動條
         ScrollPane scroll = new ScrollPane(medicPane);
         //使初始捲動面板的Viewport寬度與TilePane寬度吻合，如此便不會出現水平捲動軸
